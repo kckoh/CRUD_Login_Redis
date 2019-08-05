53eafe0a5ef49e18fb366c527efef88132ef76bf
kckoh <kyungcheolkoh@gmail.com>
2019-08-05 14:03:20 +0000

    adding a description

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Simple use of CRUD and caching
-The purpose is to learn how to use CRUD with registration and login. I have also implemented caching mechanism which i used redis. Lastly, i used mysql to store data.
-
-
+The purpose is to learn how to use CRUD with registration and login. I have also implemented caching mechanism which i used redis. Lastly, i used mysql for database to store data.
 
 
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const hbs = require("hbs");
 //use bodyParser middleware
 const bodyParser = require("body-parser");
 
-
+// database
 const conn = require("./public/config/config.js")
 
 var session = require('express-session');
@@ -19,6 +19,7 @@ var session = require('express-session');
 
 const app = express();
 
+// routes
 const product = require("./public/route/product.js")
 
 const login = require("./public/route/login.js")
@@ -29,10 +30,12 @@ const post = require("./public/route/post.js")
 
 const home = require("./public/route/home.js")
 
+
+
 //connect to database
 conn.connect(err => {
-  if (err) throw err;
-  console.log("Mysql Connected...");
+    if (err) throw err;
+    console.log("Mysql Connected...");
 });
 
 //set views file
@@ -40,39 +43,39 @@ app.set("views", path.join(__dirname, "views"));
 //set view engine
 app.set("view engine", "hbs");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 //set public folder as static folder for static file
 app.use("/assets", express.static(__dirname + "/public"));
 
 var redis = require("redis"),
     client = redis.createClient(),
-	RedisStore = require('connect-redis')(session);
+    RedisStore = require('connect-redis')(session);
 
 
 //using sessesion
 app.use(session({
-	secret: 'secret',
-	store: new RedisStore({
-        client: client, 
+    secret: 'secret',
+    store: new RedisStore({
+        client: client,
         host: 'localhost',
         port: 6379,
-        prefix : "session:",
-        db : 0,
+        prefix: "session:",
+        db: 0,
         saveUninitialized: false,
         resave: false
     }),
 
-	resave: true,
-	saveUninitialized: true
-	
+    resave: true,
+    saveUninitialized: true
+
 }));
 
 
 
-// if you'd like to select database 3, instead of 0 (default), call
-// client.select(3, function() { /* ... */ });
 
-client.on("error", function (err) {
+client.on("error", function(err) {
     console.log("Error " + err);
 });
 
@@ -96,5 +99,5 @@ app.use("/", post);
 
 //server listening
 app.listen(3000, () => {
-  console.log("Server is running at port 3000");
+    console.log("Server is running at port 3000");
 });
